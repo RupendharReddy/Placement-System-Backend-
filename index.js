757b4a463b0e2aa82f14aa44dfaf289f1ee41699
VARUGU RUPENDHAR REDDY <varugurupendharreddy@gmail.com>
2025-01-30 20:05:35 +0530

    Added the routing for the api's and for DataBase connection

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 const express = require('express');
-const mysql = require('mysql2');
 // const bodyparser = require('body-parser');  // used for parsing JSON means req.body it will convert the JSON data into javascript object
 const bcrypt = require('bcrypt');   // used for hashing
-
+const db = require('./databaseConnection');
 const app = express();
 // app.use(bodyparser.json());
+// built-in middleware for json 
 app.use(express.json());
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    password: 'root',
-    database: 'placementSystem'
-})
-db.connect((err) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log('Connected to database');
-    }
-})
+app.use(express.urlencoded({ extended: false }));
 const port = 3000;
+
+app.use('/register',require('./routes/Authentication/register'));
 
 app.get('/', (req, res) => {
     try {
@@ -39,52 +27,18 @@ app.get('/', (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-app.post('/register',async(req,res)=>{
-    try {
-        const { rollnumber, name, dob, gender, address, gmail, password, phone } = req.body;
-        if (!rollnumber || !name || !dob || !gender || !address || !gmail || !password || !phone) {
-            return res.status(400).json({ error: 'All fields are required' });
-        }
-        // hashing the password takes time so async is used it takes 2arguments (plainText,saltRounds) 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const values=[rollnumber, name, dob, gender, address, gmail, hashedPassword, phone]
-        const query = `INSERT INTO student_details (rollnumber, name, dob, gender, address, gmail, password, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-        db.query(query,values,(err,result)=>{
-            if(err)
-            {
-                console.error('Database error:', err);
-                return res.status(500).json({ error: 'Failed to insert data' });
-            }
-            else{
-                console.log("Data inserted successfully");
-                res.status(201).json({ message: 'Data inserted successfully', id: result.insertId});
-            }
-        })
-    } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
-    }
-})
-app.get('/show',(req,res)=>{
-    try {
-        const tablename=req.body.tablename;
-    if (!tablename || typeof tablename !== 'string') {
-        return res.status(400).send("Invalid or missing table name.");
-    }
-    const query=`select * from ${tablename}`;
-    db.query(query,(err,result)=>{
+app.delete('/',(req,res)=>{
+    const id=req.body.id;
+    db.query(`delete from student_details where id=${id}`,(err,result)=>{
         if(err)return res.status(500).send("Error executing the query.");
         else{
             console.log(result);
             res.json({
-                message: `${tablename} Data is:`,
+                message: `Data is:`,
                 data: result,
             });
         }
     })
-    } catch (error) {
-        console.error('Server error:', err);
-        res.status(500).json({ error: 'Internal server error' });
-    }
 })
 
 app.listen(port, () => {
---
+++ routes/Authentication/register.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const db = require('../../databaseConnection');
+
+router.route('/')
+
+    .post(async(req,res)=>{
+        try {
+            const { rollnumber, name, dob, gender, address, gmail, password, phone } = req.body;
+            if (!rollnumber || !name || !dob || !gender || !address || !gmail || !password || !phone) {
+                return res.status(400).json({ error: 'All fields are required' });
+            }
+            // hashing the password takes time so async is used it takes 2arguments (plainText,saltRounds) 
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const values=[rollnumber, name, dob, gender, address, gmail, hashedPassword, phone]
+            const query = `INSERT INTO student_details (rollnumber, name, dob, gender, address, gmail, password, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+            db.query(query,values,(err,result)=>{
+                if(err)
+            {
+                console.error('Database error:', err);
+                return res.status(500).json({ error: 'Failed to insert data' });
+            }
+            else{
+                console.log("Data inserted successfully");
+                res.status(201).json({ message: 'Data inserted successfully', id: result.insertId});
+            }
+        })
+        } catch (error) {
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    })
+    .get((req, res) => {
+        try {
+            const { gmail, password } = req.body;
+            if (!gmail || !password) {
+                return res.status(400).json({ error: 'Email and password are required' });
+            }
+
+            console.log(gmail, password);
+            const query = `SELECT * FROM student_details WHERE gmail = ?`;
+
+            db.query(query, [gmail], (err, result) => {
+                if (err) {
+                    console.error('Database error:', err);
+                    return res.status(500).send("Error executing the query.");
+                }
+
+                if (result.length === 0) {
+                    return res.status(401).json({ error: "Invalid credentials" });
+                }
+
+                const hashedPassword = result[0].password;
+                const isMatch = bcrypt.compareSync(password, hashedPassword);
+
+                if (isMatch) {
+                    console.log("Passwords match (Sync):", isMatch);
+                    return res.json({
+                        message: "Login successful",
+                        data: result[0],
+                    });
+                } else {
+                    return res.status(401).json({ error: "Invalid credentials" });
+                }
+            });
+
+        } catch (error) {
+            console.error('Server error:', error);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    });
+
+module.exports=router
